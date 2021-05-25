8e76d804e855d84ede7ffe7731eecf4771d75a0f
Raymond Chong <mathsgod@yahoo.com>
2021-05-25 16:46:48 +0800

    init

--- pages/User/ae.php
+++ pages/User/ae.php
@@ -1,5 +1,25 @@
-<vue>
-    <el-form>
-        <el-input></el-input>
-    </el-form>
-</vue>
+<el-card>
+    {{form|raw}}
+</el-card>
+
+<?php
+
+return ["get" => function (VX $context) {
+
+    $form = $context->createForm();
+
+    $form->add("Username")->input("username")->required();
+    $form->add("First name")->input("first_name")->required();
+    $form->add("Last name")->input("last_name");
+    $form->add("Password")->password("password")->required();
+
+    $form->setAction($context->route->path);
+
+    $this->form = $form;
+}, "post" => function (VX $context) {
+
+    
+    print_R($context->req->getParsedBody());
+
+    print_r($context);
+}];
