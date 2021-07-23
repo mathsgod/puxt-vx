c9fbd2b47886500c3402f64e35bcaff5a1e0f304
Raymond Chong <mathsgod@yahoo.com>
2021-07-23 16:13:52 +0800

    update

--- pages/System/package.php
+++ pages/System/package.php
@@ -1,13 +1,22 @@
+{{table|raw}}
 <?php
 
 /**
  * Created by: Raymond Chong
  * Date: 2021-07-23 
  */
+
+use VX\UI\EL\Table;
+
 return new class
 {
     function get(VX $vx)
     {
+        $table = new Table;
+        $table->setData(iterator_to_array($this->data()));
+        $table->addColumn("Name", "name")->sortable();
+        $table->addColumn("Version", "version")->sortable();
+        $this->table = $table;
     }
 
     function data()
