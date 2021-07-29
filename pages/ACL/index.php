635eb141056844b06452aab045eaa1fa374ff121
Raymond Chong <mathsgod@yahoo.com>
2021-07-29 18:04:42 +0800

    update

--- pages/ACL/index.php
+++ pages/ACL/index.php
@@ -1,15 +1,42 @@
-<vx-card>
-    <vx-card-body>{{tab|raw}}</vx-card-body>
-</vx-card>
-
+{{table|raw}}
 <?php
+
+use VX\ACL;
 
 return new class
 {
     public function get(VX $vx)
     {
-        $tab = $vx->ui->createTab();
-        $tab->add("All ACL", "list");
-        $this->tab = $tab;
+
+        $table = $vx->ui->createTable("ds");
+
+        $a = $table->addActionColumn();
+        $a->addDelete();
+
+        $table->add("Module", "module")->sortable()->searchable();
+        $table->Add("Path", "path")->sortable()->searchable();
+        $table->Add("Action", "action")->sortable()->searchable();
+        $table->Add("User", "user_id");
+        $table->Add("UserGroup", "usergroup_id");
+        $table->Add("Special User", "special_user");
+        $table->Add("Value", "value")->sortable();
+        $table->add("Type", "type")->sortable();
+
+        $this->table = $table;
+    }
+
+    public function ds(VX $vx)
+    {
+        $rt = $vx->ui->createTableResponse();
+        $rt->source = ACL::Query();
+        $rt->add("usergroup_id", "UserGroup()");
+        $rt->add("module");
+        $rt->add("path");
+        $rt->add("action");
+        $rt->add("user_id");
+        $rt->add("special_user");
+        $rt->add("value");
+        $rt->add("type");
+        return $rt;
     }
 };
