31b85490abc398a9274e82c6b01abe8c0307fcb2
Raymond Chong <mathsgod@yahoo.com>
2021-07-30 10:42:29 +0800

    mail log

--- class/VX/Mailer.php
+++ class/VX/Mailer.php
@@ -7,9 +7,27 @@ use PHPMailer\PHPMailer\PHPMailer;
 class Mailer extends PHPMailer
 {
 
-    public function __construct()
+    public function __construct($exceptions = true)
     {
-        parent::__construct(true);
+        parent::__construct($exceptions);
         $this->CharSet = "UTF-8";
+    }
+
+    public function send()
+    {
+        foreach ($this->to as $to) {
+            $l = new MailLog;
+            $l->subject = $this->Subject;
+            $l->from = $this->From;
+            $l->from_name = $this->FromName;
+            $l->to = $to[0];
+            $l->to_name = $to[1];
+            $l->body = $this->Body;
+            $l->altbody = $this->AltBody;
+            $l->host = $this->Host;
+            $l->save();
+        }
+
+        return parent::send();
     }
 }
