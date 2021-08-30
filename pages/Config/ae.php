0bc2fe7c719fe84a4f6496f988b459739b743fd2
Raymond Chong <mathsgod@yahoo.com>
2021-08-30 13:18:00 +0800

    update

--- pages/Config/ae.php
+++ pages/Config/ae.php
@@ -3,13 +3,18 @@
 
 use VX\UserGroup;
 
-return [
-    "get" => function (VX $vx) {
-
+/**
+ * Created by: Raymond Chong
+ * Date: 2021-08-30 
+ */
+return new class
+{
+    function get(VX $vx)
+    {
         $f = $vx->ui->createForm();
         $f->add("Name")->input("name")->required();
         $f->add("Value")->input("value");
 
         $this->f = $f;
     }
-];
+};
