f7589cf589b3bc8895877957222380eb27fd297f
Raymond Chong <mathsgod@yahoo.com>
2021-09-27 17:38:30 +0800

    el tooltip locaion

--- class/VX/UI/TableActionColumn.php
+++ class/VX/UI/TableActionColumn.php
@@ -17,7 +17,7 @@ class TableActionColumn extends TableColumn
         $node->append($tooltip = new HTMLElement("el-tooltip"));
         $tooltip->setAttribute("effect", "dark");
         $tooltip->setAttribute("content", $this->translator->trans("View"));
-        $tooltip->setAttribute("placement", "top");
+        $tooltip->setAttribute("placement", "left");
 
 
         $tooltip->append($icon = new Icon());
@@ -59,7 +59,7 @@ class TableActionColumn extends TableColumn
         $node->append($tooltip = new HTMLElement("el-tooltip"));
         $tooltip->setAttribute("effect", "dark");
         $tooltip->setAttribute("content", $this->translator->trans("Delete"));
-        $tooltip->setAttribute("placement", "top");
+        $tooltip->setAttribute("placement", "right");
 
 
         $tooltip->append($icon = new Icon());
