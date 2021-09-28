131ba066b78b3abcfab4717fbca59c25d5409c6c
Raymond Chong <mathsgod@yahoo.com>
2021-09-28 12:38:39 +0800

    update

--- pages/Dashboard/index.php
+++ pages/Dashboard/index.php
@@ -1,25 +1,9 @@
-<vue>
-<router-link to="/User/ae">user</router-link>
-</vue>
-
-
+Dashboard
 <?php
 
 return new class
 {
     public function get(VX $vx)
     {
-
-
-        
-        return;
-        $modules = $vx->getModules();
-
-        $menu = new VX\Menu();
-        foreach ($modules as $m) {
-            $menu->addModule($m);
-        }
-
-        outp($menu->getMenuByUser($vx->user));
     }
 };
