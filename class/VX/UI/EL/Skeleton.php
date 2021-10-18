67b2b6043b6211e6f197f271454bb55b2c89a591
Raymond Chong <mathsgod@yahoo.com>
2021-10-18 12:12:56 +0800

    el-skeleton

---
+++ class/VX/UI/EL/Skeleton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace VX\UI\EL;
+
+use P\HTMLElement;
+
+class Skeleton extends HTMLElement
+{
+    function __construct()
+    {
+        parent::__construct("el-skeleton");
+    }
+}
