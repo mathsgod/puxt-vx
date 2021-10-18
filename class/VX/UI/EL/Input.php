a88e325e68748c02fae5969a3de0d29b20b026db
Raymond Chong <mathsgod@yahoo.com>
2021-10-18 13:31:28 +0800

    input

--- class/VX/UI/EL/Input.php
+++ class/VX/UI/EL/Input.php
@@ -4,14 +4,64 @@ namespace VX\UI\EL;
 
 class Input extends FormItemElement
 {
-    public function __construct()
+    function __construct()
     {
         parent::__construct("el-input");
     }
 
-    public function placeholder(string $placeholder)
+    /**
+     * type of input
+     */
+    function setType(string $type)
+    {
+        $this->setAttribute("type", $type);
+    }
+
+    /**
+     * same as maxlength in native input
+     */
+    function setMaxlength(int $length)
+    {
+        $this->setAttribute(":maxlength", $length);
+    }
+
+    /**
+     * same as minlength in native input
+     */
+    function setMinlength(int $length)
+    {
+        $this->setAttribute(":minlength", $length);
+    }
+
+    /**
+     * whether show word count，only works when type is 'text' or 'textarea'
+     */
+    function setShowWordLimit(bool $show_word_limit)
+    {
+        $this->setAttribute("show-word-limit", $show_word_limit);
+    }
+
+    /**
+     * placeholder of Input
+     */
+    function setPlaceholder(string $placeholder)
     {
         $this->setAttribute("placeholder", $placeholder);
-        return $this;
+    }
+
+    /**
+     * whether to show clear button
+     */
+    function setClearable(bool $clearable)
+    {
+        $this->setAttribute("clearable", $clearable);
+    }
+
+    /**
+     * whether to show toggleable password input
+     */
+    function setShowPassword(bool $show)
+    {
+        $this->setAttribute("show-password", $show);
     }
 }
