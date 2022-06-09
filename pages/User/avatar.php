1a82131cafb3894b8eaa7354013b92d1d1bec6ba
Raymond Chong <mathsgod@yahoo.com>
2022-06-09 11:20:44 +0800

    update

---
+++ pages/User/avatar.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by: Raymond Chong
+ * Date: 2022-06-09 
+ */
+return new class
+{
+    function get(VX $vx)
+    {
+
+        $user = $vx->user;
+        if ($user->image === null) {
+            header('Content-Type: image/png');
+            readfile(dirname(__DIR__, 2) . "/www/images/user.png");
+            die();
+        }
+    }
+};
