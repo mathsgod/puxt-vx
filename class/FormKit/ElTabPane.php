31f501058275ca29437ef4610c9bdb308e3bce71
Raymond Chong <mathsgod@yahoo.com>
2023-02-23 12:00:05 +0800

    el tabs

---
+++ class/FormKit/ElTabPane.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace FormKit;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ElTabPane extends ComponentNode
+{
+    public function __construct(array $props = [], ?TranslatorInterface $translator = null)
+    {
+        parent::__construct("ElTabPane", $props, $translator);
+    }
+
+    /**
+     * title of the tab
+     */
+    public function label(string $label)
+    {
+        $this->setProp('label', $label);
+        return $this;
+    }
+
+    /**
+     * whether Tab is disabled
+     */
+    public function disabled(bool $disabled = true)
+    {
+        $this->setProp('disabled', $disabled);
+        return $this;
+    }
+
+    /**
+     * identifier corresponding to the name of Tabs, representing the alias of the tab-pane
+     */
+    public function name(string $name)
+    {
+        $this->setProp('name', $name);
+        return $this;
+    }
+
+    /**
+     * whether Tab is closable
+     */
+    public function closable(bool $closable = true)
+    {
+        $this->setProp('closable', $closable);
+        return $this;
+    }
+
+    /**
+     * whether Tab is lazy rendered
+     */
+    public function lazy(bool $lazy = true)
+    {
+        $this->setProp('lazy', $lazy);
+        return $this;
+    }
+}
---
+++ class/FormKit/ElTabs.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace FormKit;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ElTabs extends ComponentBaseNode
+{
+    public function __construct(array $props = [], ?TranslatorInterface $translator = null)
+    {
+        parent::__construct("ElTabs", $props, $translator);
+    }
+
+    /**
+     * type of Tab
+     */
+    function type(string $type)
+    {
+        $this->setProp('type', $type);
+        return $this;
+    }
+
+    /**
+     * whether Tab is closable
+     */
+    function closable(bool $closable = true)
+    {
+        $this->setProp('closable', $closable);
+        return $this;
+    }
+
+    /**
+     * whether Tab is addable
+     */
+    function addable(bool $addable = true)
+    {
+        $this->setProp('addable', $addable);
+        return $this;
+    }
+
+    /**
+     * whether Tab is addable and closable
+     */
+    function editable(bool $editable = true)
+    {
+        $this->setProp('editable', $editable);
+        return $this;
+    }
+
+    /**
+     * position of tabs
+     */
+    function tabPosition(string $tabPosition)
+    {
+        $this->setProp('tab-position', $tabPosition);
+        return $this;
+    }
+
+    /**
+     * whether width of tab automatically fits its container
+     */
+    function stretch(bool $stretch = true)
+    {
+        $this->setProp('stretch', $stretch);
+        return $this;
+    }
+}
