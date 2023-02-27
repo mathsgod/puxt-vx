d8c37b82b170fa04a9e604f4ebc64b8d2af2d2ff
Raymond Chong <mathsgod@yahoo.com>
2023-02-27 15:32:22 +0800

    menuitem

--- class/FormKit/ElMenuItem.php
+++ class/FormKit/ElMenuItem.php
@@ -4,7 +4,7 @@ namespace FormKit;
 
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-class ElMenuItem extends ComponentBaseNode
+class ElMenuItem extends ComponentNode
 {
     public function __construct(array $property = [], ?TranslatorInterface $translator = null)
     {
