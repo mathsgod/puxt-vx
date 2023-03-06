4d997278b2d0c27e69a1bf80d23b18ae88182207
Raymond Chong <mathsgod@yahoo.com>
2023-03-06 17:56:05 +0800

    ellink

--- class/FormKit/Element/ElLink.php
+++ class/FormKit/Element/ElLink.php
@@ -14,31 +14,31 @@ class ElLink extends ComponentNode
 
     public function type(string $type)
     {
-        $this->setProperty('type', $type);
+        $this->props['type'] = $type;
         return $this;
     }
 
     public function underline(bool $underline)
     {
-        $this->setProperty('underline', $underline);
+        $this->props['underline'] = $underline;
         return $this;
     }
 
     public function disabled(bool $disabled)
     {
-        $this->setProperty('disabled', $disabled);
+        $this->props['disabled'] = $disabled;
         return $this;
     }
 
     public function href(string $href)
     {
-        $this->setProperty('href', $href);
+        $this->props['href'] = $href;
         return $this;
     }
 
     public function icon(string $icon)
     {
-        $this->setProperty('icon', $icon);
+        $this->props['icon'] = $icon;
         return $this;
     }
 }
