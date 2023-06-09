2edb45516465ad980ebfd2f3483b21efefd668ee
Raymond Chong <mathsgod@yahoo.com>
2023-06-09 13:25:22 +0800

    ✨ 新增(filesystem.php)：新增檔案系統功能，可下載指定索引的檔案系統 🚀 更新(setting.yml)：新增檔案系統功能至系統設定選單

---
+++ pages/System/filesystem.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @author Raymond Chong
+ * @date 2023-06-09 
+ */
+
+use Laminas\Diactoros\Response\EmptyResponse;
+use Laminas\Diactoros\ResponseFactory;
+use League\Flysystem\StorageAttributes;
+
+return new class
+{
+    function download(VX $vx)
+    {
+        $index = $vx->_get["index"];
+        $fs = $vx->getFileSystem($index);
+
+        if ($fs) {
+            $files = $fs->listContents("/", true)->filter(function (StorageAttributes $attr) {
+                return $attr->isFile();
+            })->toArray();
+
+
+            //create temp file for zip
+            $temp_file = tempnam(sys_get_temp_dir(), "vx");
+
+            //create zip file
+            $zip = new ZipArchive();
+            $zip->open($temp_file, ZipArchive::CREATE);
+
+            foreach ($files as $file) {
+                $zip->addFromString($file->path(), $fs->read($file->path()));
+            }
+
+            $zip->close();
+
+            $resp = (new ResponseFactory)->createResponse();
+            $resp->getBody()->write(file_get_contents($temp_file));
+            $resp = $resp->withHeader("Content-Type", "application/octet-stream");
+            $resp = $resp->withHeader("Content-Disposition", "attachment; filename=download.zip");
+            $resp = $resp->withHeader("Content-Length", filesize($temp_file));
+            $resp = $resp->withHeader("Pragma", "no-cache");
+            $resp = $resp->withHeader("Expires", "0");
+
+
+            return $resp;
+        }
+    }
+
+    function get(VX $vx)
+    {
+        $schema = $vx->createSchema();
+
+        $fm = [];
+        $i = 0;
+        while ($_ENV["VX_FILE_MANAGER_{$i}"]) {
+            $fm[] = [
+                "type" => $_ENV["VX_FILE_MANAGER_{$i}"],
+                "download" => "/api/System/filesystem?_entry=download&index=$i"
+            ];
+            $i++;
+        }
+
+        if (count($fm) == 0) {
+            $fm[] = ["type" => "local", "download" => "/api/System/filesystem?_entry=download&index=0"];
+        }
+
+        $table = $schema->addElTable();
+        $table->data($fm);
+        $table->addColumn()->label("Type")->prop("type");
+
+        $column = $table->addColumn()->label("Action")->prop("action");
+
+        $a = $column->addElement("a")->attr("href", '$row.download');;
+        $a->addButton("Download")->type("primary")->icon("el-icon-download");
+
+
+
+
+        return $schema;
+    }
+};
