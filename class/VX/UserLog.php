3521ea1ffc1a308121d55d0100403edf30462d8b
Raymond Chong <mathsgod@yahoo.com>
2023-11-20 12:05:23 +0800

    🔨 修復(UserLog.php)：修正計算登出時間的方式，改為使用設定中的存取權杖過期時間 📝 文件(UserLog.php)：更新getLogoutTime方法以使用設定中的存取權杖過期時間計算登出時間

--- class/VX/UserLog.php
+++ class/VX/UserLog.php
@@ -6,6 +6,7 @@ class UserLog extends Model
 {
     public function getLogoutTime()
     {
+        $vx = self::$_vx;
         if ($this->logout_dt) {
             return $this->logout_dt;
         }
@@ -14,9 +15,9 @@ class UserLog extends Model
             return null;
         }
 
-        //cal by last access time + session timeout(8 hours)
+        //cal by last access time + session timeout(from setting)
         $last_access = strtotime($this->last_access_time);
-        $logout = $last_access + 8 * 60 * 60;
+        $logout = $last_access + $vx->config["VX"]["access_token_expire"];
 
         if ($logout > time()) {
             return null;
